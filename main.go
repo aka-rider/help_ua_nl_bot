d4a0d25f9e5da0e1f229f9658a90b30499e6629a
Iurii Krasnoshchok <hello@iurii.net>
2022-03-02 01:45:34 +0100

    added call center menu; volunteer/humanitarian branch fully translated; intro message translated

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"os"
 	"time"
@@ -12,16 +13,32 @@ import (
 )
 
 const (
-	defaultLocale   = "ua"
-	greetings       = "Привіт! Ми — українці в Нідерландах\nБажаєте допомогти Україні?"
-	defaultPollFreq = 3 * time.Second
-	bankDetails     = `
+	defaultLocale = "ua"
+
+	startMsg_ua = `Відправте /start щоб почати спочатку`
+	startMsg_en = `Enter /start to begin or restart`
+
+	greetings_ua = `
+Привіт! Ми українці в Нідерландах. Бажаєте допомогти Україні?
+
+❗️ Не поширюйте цей чат бот за межами Нідерландів
+` + startMsg_ua
+
+	greetings_en = `
+Hello! We are Ukrainians in the Netherlands. Do you want to help Ukraine?
+
+❗️ Please do not share this chat bot outside the Netherlands
+` + startMsg_en
+
+	bankDetails = `
 Stichting Oekraïners in Nederland
 IBAN: NL 97 INGB 0006 5104 66
 SWIFT/BIC: INGBNL2AXXX
 Bank: ING Bank
 Address: Bijlmerdreef 106, 1102 MG Amsterdam, Netherlands
 `
+
+	defaultPollFreq = 3 * time.Second
 )
 
 func Run(token string) {
@@ -50,7 +67,11 @@ func Run(token string) {
 			}),
 			flow.NewNode("humanitarian", forward).
 				Add("clothes", func(e *menu.Node, c *tb.Callback) int {
-					e.SetCaption(c, "ОДЯГ ТА ВЗУТТЯ ПОКИ НЕ ПРИЙМАЮТЬСЯ")
+					if e.GetLanguage(c) == "ua" {
+						e.SetCaption(c, "НАРАЗІ НЕМАЄ ПОТРЕБИ В ОДЯГУ ТА ВЗУТТІ. БУДЬ ЛАСКА, НЕ НЕСІТЬ.")
+					} else {
+						e.SetCaption(c, "THERE IS NO NEED FOR CLOTHES AND SHOES")
+					}
 					return menu.Forward
 				}).
 				AddWith("essentials", forward,
@@ -101,6 +122,10 @@ func Run(token string) {
 					e.SetCaption(c, "https://docs.google.com/forms/d/e/1FAIpQLSfdT5nlzQiq2SgxEYK5sZ60ZmirugGsn85crxcOc4Fv-cYyVw/viewform")
 					return menu.Forward
 				}).
+				Add("call-center", func(e *menu.Node, c *tb.Callback) int {
+					e.SetCaption(c, "https://docs.google.com/forms/d/e/1FAIpQLScUkkX7ZsI2EV-KgXjdj8PmWDpE4SmbSqNUEtxfLUmas2Gizg/viewform")
+					return menu.Forward
+				}).
 				Add("back", back),
 			flow.NewNode("coordination", func(e *menu.Node, c *tb.Callback) int {
 				e.SetCaption(c, "https://forms.gle/KKCoYbeqp5PbUxA27")
@@ -125,7 +150,7 @@ func Run(token string) {
 	flow.Build("ua").Build("en")
 
 	bot.Handle("/start", func(m *tb.Message) {
-		err = flow.Start(m.Sender, greetings, defaultLocale)
+		err = flow.Start(m.Sender, greetings_ua, defaultLocale)
 		if err != nil {
 			log.Println("failed to display the menu:", err)
 		}
@@ -139,26 +164,42 @@ func switchLanguage(e *menu.Node, c *tb.Callback) int {
 	log.Println(c.Sender.Recipient(), "press", e.GetText())
 	if e.GetLanguage(c) == "en" {
 		e.SetLanguage(c, "ua")
+		e.SetCaption(c, greetings_ua)
 	} else {
 		e.SetLanguage(c, "en")
+		e.SetCaption(c, greetings_en)
 	}
 	return menu.Forward
 }
 
 func forward(e *menu.Node, c *tb.Callback) int {
 	log.Println(c.Sender.Recipient(), "->", e.GetText())
-	e.SetCaption(c, "...")
+	if e.GetLanguage(c) == "ua" {
+		e.SetCaption(c, startMsg_ua)
+	} else {
+		e.SetCaption(c, startMsg_en)
+	}
 	return menu.Forward
 }
 
 func back(e *menu.Node, c *tb.Callback) int {
 	log.Println(c.Sender.Recipient(), "<-", e.GetText())
-	e.SetCaption(c, "...")
+	if e.GetLanguage(c) == "ua" {
+		e.SetCaption(c, startMsg_ua)
+	} else {
+		e.SetCaption(c, startMsg_en)
+	}
 	return menu.Back
 }
 
 func main() {
-	//token := os.Getenv("HELP_UA_NL_BOT_TOKEN")
-	token := os.Getenv("TEST_HELP_UA_NL_BOT_TOKEN")
+	debug := flag.Bool("debug", false, "Use test bot token")
+	flag.Parse()
+	var token string
+	if *debug {
+		token = os.Getenv("TEST_HELP_UA_NL_BOT_TOKEN")
+	} else {
+		token = os.Getenv("HELP_UA_NL_BOT_TOKEN")
+	}
 	Run(token)
 }
